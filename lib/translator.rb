c0028e7756639ff83f491e0e4d559e9a516a7eed
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-31 16:44:41 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,44 @@
 # require modules here
+require "yaml"
+require "pp"
 
-def load_library
-  # code goes here
+def transform_data(data)
+  transformed_data = {:get_meaning => {}, :get_emoticon => {}}
+
+  data.each do |key, value|
+    transformed_data[:get_meaning].merge!({value[1] => key})
+    transformed_data[:get_emoticon].merge!({value[0] => value[1]})
+  end
+
+  return transformed_data
 end
 
-def get_japanese_emoticon
-  # code goes here
+def load_library(file_path)
+  data = nil
+
+  data = YAML.load_file(file_path)
+  data = transform_data(data)
+  #pp data
+
+  return data
 end
 
-def get_english_meaning
-  # code goes here
+def get_japanese_emoticon(file_path, english_emoticon)
+  emoticon_library = load_library (file_path)
+
+  if emoticon_library[:get_emoticon].has_key?(english_emoticon) then
+    return emoticon_library[:get_emoticon][english_emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
+end
+
+def get_english_meaning(file_path, japanese_emoticon)
+  emoticon_library = load_library (file_path)
+
+  if emoticon_library[:get_meaning].has_key?(japanese_emoticon) then
+    return emoticon_library[:get_meaning][japanese_emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
